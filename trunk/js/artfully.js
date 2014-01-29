f64474af73bceb8274410004113fede9dd9bc295
meandthewallaby <hewittej+github@gmail.com>
2014-01-28 21:42:43 -0500

    Removed time zone mapping from artfully.js

--- trunk/js/artfully.js
+++ trunk/js/artfully.js
@@ -401,9 +401,7 @@ function (window, document, undefined) {
 	var myData;
 
         function prep(data) {
-	    var es = artfully.utils.modelizeArray(data, artfully.models.artfully_events);
-	    es.addTZ();
-	    return es;
+	    return artfully.utils.modelizeArray(data, artfully.models.artfully_events);
         }
 
         function render(year, month, dom_id) {
@@ -640,8 +638,7 @@ function (window, document, undefined) {
 			    first_showtime: this[i].shows[0].show_time,
 			    last_showtime: this[i].shows[numShows-1].show_time,
 			    next_showtime: nextShowtime,
-			    next_datetime: nextDatetime,
-			    time_zone: rubyTZMap[this[i].venue.time_zone]
+			    next_datetime: nextDatetime
 			};
 			jQuery.extend(this[i], showtimes);
 		    }
@@ -659,13 +656,6 @@ function (window, document, undefined) {
         }), modelCache.next_events
     }, artfully_events = function () {
         return modelCache.artfully_events === undefined && (modelCache.artfully_events = {
-	    addTZ: function() {
-		for(var i = 0; i < this.length; i++) {
-		    var tz = this[i].venue != null ? rubyTZMap[this[i].venue.time_zone] : null;
-		    var tzObj = { time_zone: tz };
-		    jQuery.extend(this[i], tzObj);
-		}
-	    }
         }), modelCache.artfully_events
     }, {
         chart: chart,
@@ -678,154 +668,3 @@ function (window, document, undefined) {
 	artfully_events: artfully_events
     }
 }();
-
-// Quick TimeZone mapping from Ruby time zones to IANA/Olson time zones
-var rubyTZMap = 
-{
-    "International Date Line West" : "Pacific/Midway",
-    "Midway Island" : "Pacific/Midway",
-    "American Samoa" : "Pacific/Pago_Pago",
-    "Hawaii" : "Pacific/Honolulu",
-    "Alaska" : "America/Juneau",
-    "Pacific Time (US & Canada)" : "America/Los_Angeles",
-    "Tijuana" : "America/Tijuana",
-    "Mountain Time (US & Canada)" : "America/Denver",
-    "Arizona" : "America/Phoenix",
-    "Chihuahua" : "America/Chihuahua",
-    "Mazatlan" : "America/Mazatlan",
-    "Central Time (US & Canada)" : "America/Chicago",
-    "Saskatchewan" : "America/Regina",
-    "Guadalajara" : "America/Mexico_City",
-    "Mexico City" : "America/Mexico_City",
-    "Monterrey" : "America/Monterrey",
-    "Central America" : "America/Guatemala",
-    "Eastern Time (US & Canada)" : "America/New_York",
-    "Indiana (East)" : "America/Indiana/Indianapolis",
-    "Bogota" : "America/Bogota",
-    "Lima" : "America/Lima",
-    "Quito" : "America/Lima",
-    "Atlantic Time (Canada)" : "America/Halifax",
-    "Caracas" : "America/Caracas",
-    "La Paz" : "America/La_Paz",
-    "Santiago" : "America/Santiago",
-    "Newfoundland" : "America/St_Johns",
-    "Brasilia" : "America/Sao_Paulo",
-    "Buenos Aires" : "America/Argentina/Buenos_Aires",
-    "Montevideo" : "America/Montevideo",
-    "Georgetown" : "America/Guyana",
-    "Greenland" : "America/Godthab",
-    "Mid-Atlantic" : "Atlantic/South_Georgia",
-    "Azores" : "Atlantic/Azores",
-    "Cape Verde Is." : "Atlantic/Cape_Verde",
-    "Dublin" : "Europe/Dublin",
-    "Edinburgh" : "Europe/London",
-    "Lisbon" : "Europe/Lisbon",
-    "London" : "Europe/London",
-    "Casablanca" : "Africa/Casablanca",
-    "Monrovia" : "Africa/Monrovia",
-    "UTC" : "Etc/UTC",
-    "Belgrade" : "Europe/Belgrade",
-    "Bratislava" : "Europe/Bratislava",
-    "Budapest" : "Europe/Budapest",
-    "Ljubljana" : "Europe/Ljubljana",
-    "Prague" : "Europe/Prague",
-    "Sarajevo" : "Europe/Sarajevo",
-    "Skopje" : "Europe/Skopje",
-    "Warsaw" : "Europe/Warsaw",
-    "Zagreb" : "Europe/Zagreb",
-    "Brussels" : "Europe/Brussels",
-    "Copenhagen" : "Europe/Copenhagen",
-    "Madrid" : "Europe/Madrid",
-    "Paris" : "Europe/Paris",
-    "Amsterdam" : "Europe/Amsterdam",
-    "Berlin" : "Europe/Berlin",
-    "Bern" : "Europe/Berlin",
-    "Rome" : "Europe/Rome",
-    "Stockholm" : "Europe/Stockholm",
-    "Vienna" : "Europe/Vienna",
-    "West Central Africa" : "Africa/Algiers",
-    "Bucharest" : "Europe/Bucharest",
-    "Cairo" : "Africa/Cairo",
-    "Helsinki" : "Europe/Helsinki",
-    "Kyiv" : "Europe/Kiev",
-    "Riga" : "Europe/Riga",
-    "Sofia" : "Europe/Sofia",
-    "Tallinn" : "Europe/Tallinn",
-    "Vilnius" : "Europe/Vilnius",
-    "Athens" : "Europe/Athens",
-    "Istanbul" : "Europe/Istanbul",
-    "Minsk" : "Europe/Minsk",
-    "Jerusalem" : "Asia/Jerusalem",
-    "Harare" : "Africa/Harare",
-    "Pretoria" : "Africa/Johannesburg",
-    "Moscow" : "Europe/Moscow",
-    "St. Petersburg" : "Europe/Moscow",
-    "Volgograd" : "Europe/Moscow",
-    "Kuwait" : "Asia/Kuwait",
-    "Riyadh" : "Asia/Riyadh",
-    "Nairobi" : "Africa/Nairobi",
-    "Baghdad" : "Asia/Baghdad",
-    "Tehran" : "Asia/Tehran",
-    "Abu Dhabi" : "Asia/Muscat",
-    "Muscat" : "Asia/Muscat",
-    "Baku" : "Asia/Baku",
-    "Tbilisi" : "Asia/Tbilisi",
-    "Yerevan" : "Asia/Yerevan",
-    "Kabul" : "Asia/Kabul",
-    "Ekaterinburg" : "Asia/Yekaterinburg",
-    "Islamabad" : "Asia/Karachi",
-    "Karachi" : "Asia/Karachi",
-    "Tashkent" : "Asia/Tashkent",
-    "Chennai" : "Asia/Kolkata",
-    "Kolkata" : "Asia/Kolkata",
-    "Mumbai" : "Asia/Kolkata",
-    "New Delhi" : "Asia/Kolkata",
-    "Kathmandu" : "Asia/Kathmandu",
-    "Astana" : "Asia/Dhaka",
-    "Dhaka" : "Asia/Dhaka",
-    "Sri Jayawardenepura" : "Asia/Colombo",
-    "Almaty" : "Asia/Almaty",
-    "Novosibirsk" : "Asia/Novosibirsk",
-    "Rangoon" : "Asia/Rangoon",
-    "Bangkok" : "Asia/Bangkok",
-    "Hanoi" : "Asia/Bangkok",
-    "Jakarta" : "Asia/Jakarta",
-    "Krasnoyarsk" : "Asia/Krasnoyarsk",
-    "Beijing" : "Asia/Shanghai",
-    "Chongqing" : "Asia/Chongqing",
-    "Hong Kong" : "Asia/Hong_Kong",
-    "Urumqi" : "Asia/Urumqi",
-    "Kuala Lumpur" : "Asia/Kuala_Lumpur",
-    "Singapore" : "Asia/Singapore",
-    "Taipei" : "Asia/Taipei",
-    "Perth" : "Australia/Perth",
-    "Irkutsk" : "Asia/Irkutsk",
-    "Ulaanbaatar" : "Asia/Ulaanbaatar",
-    "Seoul" : "Asia/Seoul",
-    "Osaka" : "Asia/Tokyo",
-    "Sapporo" : "Asia/Tokyo",
-    "Tokyo" : "Asia/Tokyo",
-    "Yakutsk" : "Asia/Yakutsk",
-    "Darwin" : "Australia/Darwin",
-    "Adelaide" : "Australia/Adelaide",
-    "Canberra" : "Australia/Melbourne",
-    "Melbourne" : "Australia/Melbourne",
-    "Sydney" : "Australia/Sydney",
-    "Brisbane" : "Australia/Brisbane",
-    "Hobart" : "Australia/Hobart",
-    "Vladivostok" : "Asia/Vladivostok",
-    "Guam" : "Pacific/Guam",
-    "Port Moresby" : "Pacific/Port_Moresby",
-    "Magadan" : "Asia/Magadan",
-    "Solomon Is." : "Pacific/Guadalcanal",
-    "New Caledonia" : "Pacific/Noumea",
-    "Fiji" : "Pacific/Fiji",
-    "Kamchatka" : "Asia/Kamchatka",
-    "Marshall Is." : "Pacific/Majuro",
-    "Auckland" : "Pacific/Auckland",
-    "Wellington" : "Pacific/Auckland",
-    "Nuku'alofa" : "Pacific/Tongatapu",
-    "Tokelau Is." : "Pacific/Fakaofo",
-    "Chatham Is." : "Pacific/Chatham",
-    "Samoa" : "Pacific/Apia"
-};
